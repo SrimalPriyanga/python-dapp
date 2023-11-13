5116f784ae2f2ce887373cac8a7fe3cae3f54bd1
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-13 11:15:18 +0530

    simple output exercise

---
+++ hello-world/exercise_work_with_output.py
@@ -0,0 +1,12 @@
+# Exercise - Work with output 
+
+# 1 Display today's date
+from datetime import date
+
+print("Today's Date is:" + str(date.today()))
+
+# 2 Build a unit converter
+parsecs = 11
+lightyears = parsecs * 3.26
+
+print(str(parsecs) + " Parsecs is " + str(lightyears) + " lightyears")
