6731217f7e1a8db8b79298b845a0903ca7da6b5f
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-13 11:36:06 +0530

    user input and working with numbers example

--- hello-world/collect_input.py
+++ hello-world/collect_input.py
@@ -4,3 +4,14 @@ import sys
 print(sys.argv)
 print(sys.argv[0]) # program name
 print(sys.argv[1]) # first arg
+
+# User input
+print("Welcome to the greeter program")
+name = input("Enter your name: ")
+print("Greetings " + name)
+
+# Working with numbers
+print("calculator program")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
