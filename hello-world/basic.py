5c40a231a9f4bc264758686d90bd64800a8e047b
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-13 10:45:45 +0530

    Adding operator example

--- hello-world/basic.py
+++ hello-world/basic.py
@@ -14,7 +14,12 @@ shuttle_landed_on_the_moon = "Apollo 11" #string
 
 distance_to_alpha_centauri = 4.367 # looks like a float
 
-type(distance_to_alpha_centauri) ## <class 'float'>
+type(distance_to_alpha_centauri) ## <class 'float'> check data type of a variable
 
-# Todo ..
 # Operators
+# <left side> <operator> <right side>
+# Arithmetic operators
+# Assignment operators
+left_side = 10
+right_side = 5
+left_side / right_side # 2
