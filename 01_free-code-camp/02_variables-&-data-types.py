d4ab85aa9b5f22dbceb14dfd62d848896bf32028
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-14 12:00:22 +0530

    variables and data type string, int, boolean exmpl

---
+++ 01_free-code-camp/02_variables-&-data-types.py
@@ -0,0 +1,11 @@
+user_name = "John"
+user_age = 23
+isAllowed = True
+
+print("Hi " + user_name + ", you are allowed to log-in. age = " +str(user_age) +" (> 18+)")
+
+user_name = "Srimal"
+user_age = 16
+isAllowed = False
+
+print("Hi " + user_name + ", you are not-allowed to log-in. age = " +str(user_age) +" (< 18+)")
