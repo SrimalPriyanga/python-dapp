f2520911003077fd2d7c2f748c1a65ed32535504
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-14 11:00:10 +0530

    accept console input example

---
+++ hello-world/06_exercise_work_with_input.py
@@ -0,0 +1,6 @@
+print("*** Convert Parsecs to Lightyears ***")
+parsecs_input = input("Enter the number of parsecs: ")
+
+parsecs = int(parsecs_input)
+lightyears = parsecs * 3.26
+print(str(parsecs) + " parsecs is " + str(lightyears) + " lightyears")
