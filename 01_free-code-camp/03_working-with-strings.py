c1bb71abe0cf7ad5b4298274955f269ebee72a70
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-16 11:41:50 +0530

    string basic examples

---
+++ 01_free-code-camp/03_working-with-strings.py
@@ -0,0 +1,4 @@
+print("This is example 1 string print!")
+print("This is example 2 \n new line print!") #new line using \n
+print("This is example 3 \" quotation print!") #print quotation mark using backslash as an escape character \"
+print("This is example 4 \ backslash print!") #print backslash mark as a string
