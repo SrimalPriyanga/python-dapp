82609e02d413c4065a0b569dfa7d6f4306bc78ee
Srimal Priyanga <s.priyanga22@gmail.com>
2023-11-17 10:55:33 +0530

    working with numbers

---
+++ 01_free-code-camp/04_working-with-numbers.py
@@ -0,0 +1,25 @@
+# Basic number examples + Arithmetic operations
+print(1)
+print(2.01)
+print(-3)
+print(4 + 1)
+print(5 - 2)
+print(6 * 3)
+print(7 / 4)
+print(8 * 5 + 1)
+print(9 *(6 + 2))
+print(10 % 7)
+
+print("\n==================== Examples using number variables ====================")
+lucky_num = 11
+print(lucky_num)
+print(str(lucky_num) + " :this is string converted number seven")
+
+# using Math function 
+lucky_num = -12
+print(abs(lucky_num))
+print(pow(lucky_num, 2)) # 3^2 = 9 *power
+print(min(13, 14))
+print(max(13, 14))
+print(round(15.2))
+print(round(15.8))
