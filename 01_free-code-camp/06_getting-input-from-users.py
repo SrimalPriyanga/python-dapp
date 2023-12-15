483b21d249b999af1e81e7236ff49ff00945efb7
Srimal Priyanga <s.priyanga22@gmail.com>
2023-12-15 10:19:51 +0530

    working with user inputs

---
+++ 01_free-code-camp/06_getting-input-from-users.py
@@ -0,0 +1,5 @@
+# A little chat
+name = input("Enter your name: ")
+print(name,sep="_",end=", How can I assist you today?\n")
+question = input("")
+print("Your question was: "+ question + "\nOkay.. Let's find out!")
