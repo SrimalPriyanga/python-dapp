a94de885d3c6fc3981ebfab8ef1c52c6398a5c9d
Srimal Priyanga <s.priyanga22@gmail.com>
2024-01-11 10:20:32 +0530

    simple mad libs game: input & outputs

---
+++ 01_free-code-camp/08_mad-libs-game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noun = input("Enter a Plural noun: ")
+celebrity = input("Enter a celebrity name: ")
+
+print("Roses are " + color)
+print(plural_noun + " are black")
+print("I love " + celebrity)
